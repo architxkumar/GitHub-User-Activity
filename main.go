4dae3bf60dd00371969fa34c4f392f23ed00aec2
Archit Kumar <architxkumar@proton.me>
2025-06-20 17:52:24 +0530

    feat: add GitHub user activity logging with event type, repo name and timestamp

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"GitHub-User-Activity/internal/model"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -11,5 +16,37 @@ func main() {
 		fmt.Println("Invalid number of arguments")
 	}
 	username := arguments[1]
-	fmt.Printf("Hello, %s!\n", username)
+	url := fmt.Sprintf("https://api.github.com/users/%s/events", username)
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal("Unable to connect to Github.\n", err.Error())
+	}
+	switch response.StatusCode {
+	case 200:
+		printPublicUserEvents(response.Body)
+	case 404:
+		fmt.Println("User not found")
+	default:
+		fmt.Println(response.StatusCode, response.Status)
+	}
+	if err := response.Body.Close(); err != nil {
+		log.Fatal("Unable to close response body.\n", err.Error())
+	}
+}
+func printPublicUserEvents(responseBody io.ReadCloser) {
+	body, err := io.ReadAll(responseBody)
+	if err != nil {
+		log.Fatal("Unable to read response body.\n", err.Error())
+	}
+	var eventArray []model.Event
+	err = json.Unmarshal(body, &eventArray)
+	if err != nil {
+		log.Fatal("Unable to parse JSON.\n", err.Error())
+	}
+	if len(eventArray) == 0 {
+		fmt.Println("No events found")
+	}
+	for _, event := range eventArray {
+		fmt.Println(event.EventType, " to ", event.Repo.Name, " at ", event.CreatedAt)
+	}
 }
