82e6f7d9f239c33e74dbe5c8dbc3b88bef67308b
Archit Kumar <architxkumar@proton.me>
2025-06-21 12:28:10 +0530

    refactor: update `Payload` field in `Event` struct from a interface to `payload` model

--- internal/model/event.go
+++ internal/model/event.go
@@ -3,11 +3,11 @@ package model
 import "time"
 
 type Event struct {
-	Id        string      `json:"id"`
-	EventType string      `json:"type"`
-	Actor     Actor       `json:"actor"`
-	Repo      repository  `json:"repo"`
-	Public    bool        `json:"public"`
-	CreatedAt time.Time   `json:"created_at"`
-	Payload   interface{} `json:"payload"`
+	Id        string     `json:"id"`
+	EventType string     `json:"type"`
+	Actor     Actor      `json:"actor"`
+	Repo      repository `json:"repo"`
+	Public    bool       `json:"public"`
+	CreatedAt time.Time  `json:"created_at"`
+	Payload   payload    `json:"payload"`
 }
