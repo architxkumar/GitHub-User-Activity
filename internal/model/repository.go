f1e84769dddc2312eff00088ecdc467ec5a49ec3
Archit Kumar <architxkumar@proton.me>
2025-06-27 19:35:55 +0530

    refactor(model): remove unused field `Url`from `repository` struct

--- internal/model/repository.go
+++ internal/model/repository.go
@@ -8,6 +8,4 @@ type repository struct {
 	//
 	// For example, octocat/hello-world is the name of the hello-world repository owned by the octocat personal account.
 	Name string `json:"name"`
-	// The REST API URL used to retrieve the repository object, which includes additional repository information.
-	Url string `json:"url"`
 }
