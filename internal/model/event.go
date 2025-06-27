198e03a51dbcccc5f59e41f194924431965ecf7d
Archit Kumar <architxkumar@proton.me>
2025-06-27 19:35:14 +0530

    refactor(model): remove unused fields `Actor` and `Public from Event struct

--- internal/model/event.go
+++ internal/model/event.go
@@ -8,12 +8,8 @@ type Event struct {
 	Id string `json:"id"`
 	// EventType is the type of event. Events uses PascalCase for the name.
 	EventType string `json:"type"`
-	// Actor is the user that triggered the event.
-	Actor Actor `json:"actor"`
 	// Repo is the repository object where the event occurred.
 	Repo repository `json:"repo"`
-	// Public represents whether the event is visible to all users.
-	Public bool `json:"public"`
 	// CreatedAt is the date and time when the event was triggered. It is formatted according to ISO 8601.
 	CreatedAt time.Time `json:"created_at"`
 	// Payload is the event payload object is unique to the event type.
